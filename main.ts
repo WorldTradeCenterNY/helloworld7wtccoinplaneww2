f1b14d93911a855551ca2c36b926a70178524afd
WorldTradeCenterNY <122543666+WorldTradeCenterNY@users.noreply.github.com>
2023-01-13 09:35:13 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Tortoise)
+})
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
